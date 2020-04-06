48d335912aa92d67852efbc7305c64f19e7780b5
xueyong <wszgr199@163.com>
2020-04-07 02:26:45 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <el-button type="info" @click="logout">退出</el-button>
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {}
+  },
+  methods: {
+    logout () {
+      window.sessionStorage.clear('token')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+ 
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router.js'
+
+// 引入全局样式
+import './assets/css/global.css'
+
+import './plugins/element.js'
+
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+Vue.prototype.$http = axios
+
+
+
 
 Vue.config.productionTip = false
-
+import './assets/fonts/iconfont.css'
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
